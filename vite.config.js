07c73c53b61e0bf36429e44a7e353251f3f114e8
jakemackie <jake.wmackie@gmail.com>
2024-09-14 19:11:11 +0100

    tailwindcss with vite

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,12 @@
-export default {}
+import { defineConfig } from 'vite'
+import tailwindcss from 'tailwindcss'
+
+export default defineConfig({
+  root: 'src',
+  build: { outDir: 'dist' },
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
+    },
+  },
+})
